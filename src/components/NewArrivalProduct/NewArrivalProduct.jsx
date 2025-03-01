54d329761c24444292d71effe1e695bcc653a839
alaahosnii <alaahosnii1000@gmail.com>
2025-03-02 00:34:58 +0200

    changes in new arrival section

--- src/components/NewArrivalProduct/NewArrivalProduct.jsx
+++ src/components/NewArrivalProduct/NewArrivalProduct.jsx
@@ -7,7 +7,7 @@ function NewArrivalProduct({ productImage, productType }) {
     <div className={`${productType == "playstation" ? "col-6" : "col-5"} position-relative d-flex`}>
       <img src={productImage} height={"100%"} width={"100%"} />
       <div className='d-flex flex-column position-absolute text-white' style={{ width: `${productType == "playstation" && "35%"}`, bottom: `${productType == "playstation" ? "100px" : "10px"}`, left: "30px" }}>
-        <h4>{productType}</h4>
+        <h4>{productType == "playstation5" ? "Playstation 5" : productType == "smartspeakers" ? "Smart Speakers" : "Perfumes"}</h4>
         <p style={{ fontSize: "13px", marginBottom: "0px" }}>Black and White version of the PS5 coming out on sale.</p>
         <NavLink style={{ marginBottom: "10px", color: "white" }} state={
           {
